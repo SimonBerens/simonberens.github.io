c87ed4a908451ea55f64b753dec37c44ad3debd3
sberens <sberensnyc@gmail.com>
2023-06-22 15:59:44 -0700

    add custom fonts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -15,6 +15,15 @@ export default defineConfig({
   head: [[
     'link',
     {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css', integrity: 'sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI', crossorigin: 'anonymous'}
+  ],[
+    'link',
+    {rel: 'preconnect', href: 'https://fonts.googleapis.com'}
+  ],[
+    'link',
+    {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous'}
+  ],[
+    'link',
+    {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap', crossorigin: 'anonymous'}
   ]],
   markdown: {
     config: md => {
